556f755f555eb7fb544f717d24ae10593a8de2e8
mkeller <7525285+keller-mark@users.noreply.github.com>
2022-09-23 20:52:51 -0400

    More comments

--- README.md
+++ README.md
@@ -1,14 +1,21 @@
 # compsci279r-doodle
 
+## How to open hosted version
 
-## Development
+Navigate to https://keller-mark.github.io/compsci279r-doodle/ in a web browser.
+
+## How to open locally
+
+1. Install NodeJS and NPM
+
+2. Run `npm install` to install the dependencies specified in `package.json`
+
+3. In a terminal, run the following to start the development server:
 
 ```sh
 npm run dev
 ```
 
-## Production
+## Resources
 
-```sh
-npm run build
-```
+No tutorial was used to complete this assignment. All online resources used are referenced in the comments in the code.
--- src/App.jsx
+++ src/App.jsx
@@ -12,78 +12,111 @@ const months = [
   'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
 ];
 
-const organizer = 'Jane Doe';
+// Use a constant object to name the three possible checkbox state values.
+const STATE = {
+  NO: 0,
+  YES: 1,
+  MAYBE: 2,
+};
 
+// Create a global time formatter object.
+// Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat/DateTimeFormat
+const shortTime = new Intl.DateTimeFormat("en", {
+  timeStyle: "short"
+});
+
+// Hard-code the event details, participant names, and the time slots.
+const organizer = 'Jane Doe';
+// Store a list of participant names who have already filled out the poll.
 const friends = [
   'Jane Doe',
   'John Doe',
 ];
+// Store a list of time slot objects.
 const timeslots = [
   {
+    // Use the JS date object.
+    // Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
     start: new Date("September 21, 2022 04:00:00"),
     end: new Date("September 21, 2022 08:00:00"),
     states: {
-      'John Doe': 1,
-      'Jane Doe': 0,
+      // Store the selection of each participant for this time slot.
+      'John Doe': STATE.YES,
+      'Jane Doe': STATE.NO,
     }
   },
   {
     start: new Date("December 24, 2022 10:00:00"),
     end: new Date("December 24, 2022 12:00:00"),
     states: {
-      'John Doe': 0,
-      'Jane Doe': 0,
+      'John Doe': STATE.NO,
+      'Jane Doe': STATE.NO,
     }
   },
   {
     start: new Date("December 24, 2022 12:00:00"),
     end: new Date("December 24, 2022 13:00:00"),
     states: {
-      'John Doe': 1,
-      'Jane Doe': 1,
+      'John Doe': STATE.YES,
+      'Jane Doe': STATE.YES,
     }
   },
   {
     start: new Date("December 25, 2022 13:00:00"),
     end: new Date("December 25, 2022 15:00:00"),
     states: {
-      'John Doe': 0,
-      'Jane Doe': 2,
+      'John Doe': STATE.NO,
+      'Jane Doe': STATE.MAYBE,
     }
   },
   {
     start: new Date("December 26, 2022 10:00:00"),
     end: new Date("December 26, 2022 15:00:00"),
     states: {
-      'John Doe': 0,
-      'Jane Doe': 2,
+      'John Doe': STATE.NO,
+      'Jane Doe': STATE.MAYBE,
     }
   }
 ];
 
+// Create a component to represent the "you" part of a timeslot column.
+// (The clickable upper part of each column with the date/time and the checkbox).
 function YouTimeCol(props) {
   const {
+    // Take the start and end datetime as props.
     start,
     end,
   } = props;
 
+  // Set this checkbox to disabled if the time slot
+  // is in the past (has already occurred).
   const isDisabled = start < (new Date());
-  const shortTime = new Intl.DateTimeFormat("en", {
-    timeStyle: "short"
-  });
 
+  // Set up a ref for the outer <div/> which will
+  // allow us to attach an event handler on its
+  // HTML element node.
   const colRef = useRef();
-  const [checkState, setCheckState] = useState(0);
+  // Store the state of this time slot,
+  // and initialize it to the unavailable state.
+  const [checkState, setCheckState] = useState(STATE.NO);
 
+  // Use a React effect to set up the click handler
+  // on the outer <div/>.
   useEffect(() => {
-    function clickHandler(event) {
+    // Create a click handler function.
+    function clickHandler() {
+      // Only allow the click to affect the state
+      // if the time slot is not disabled.
       if(!isDisabled) {
         setCheckState(prev => (prev + 1) % 3);
       }
     }
     if(colRef.current) {
+      // Add the event listener.
       colRef.current.addEventListener('click', clickHandler);
     }
+    // In the cleanup function of the effect, remove the
+    // event listener.
     return () => {
       if(!colRef.current) {
         return;
@@ -100,38 +133,49 @@ function YouTimeCol(props) {
   );
 }
 
+// Create a summary column component,
+// to indicate the number of participants
+// who are available (or maybe available) for the time slot.
 function SummaryTimeCol(props) {
   const {
     start,
     states
   } = props;
 
+  // Check if the timeslot is in the past.
   const isDisabled = start < (new Date());
+  // Compute the number of people who have indicated YES/MAYBE.
+  const numAvailable = sum(Object.values(states).map(v => v > 0 ? 1 : 0));
 
   return (
     <div className={clsx("datetime-col", "summary-num", { disabled: isDisabled })}>
-      <span className="info-icon"><YesIcon/> {sum(Object.values(states).map(v => v > 0 ? 1 : 0))}</span>
+      <span className="info-icon"><YesIcon/> {numAvailable}</span>
     </div>
   );
 }
 
+// Create a component to represent the time slot column
+// for others (to indicate to the user others' availability).
 function FriendTimeCol(props) {
   const {
     start,
     checkState,
   } = props;
 
+  // Check whether the time slot is in the past.
   const isDisabled = start < (new Date());
 
+  // Store a mapping from the time slot state
+  // to the icons and CSS classes to display.
   const stateToComponent = {
-    0: <NoIcon/>,
-    1: <YesIcon/>,
-    2: <MaybeIcon/>,
+    [STATE.NO]: <NoIcon/>,
+    [STATE.YES]: <YesIcon/>,
+    [STATE.MAYBE]: <MaybeIcon/>,
   };
   const stateToClassName = {
-    0: "no-primary",
-    1: "yes-primary",
-    2: "maybe-primary",
+    [STATE.NO]: "no-primary",
+    [STATE.YES]: "yes-primary",
+    [STATE.MAYBE]: "maybe-primary",
   };
   return (
     <div className="datetime-col">
@@ -142,21 +186,29 @@ function FriendTimeCol(props) {
   )
 }
 
+// Create a fake checkbox that has three different states
+// (yes, no, maybe) plus a disabled modifier.
+// This does not handle the click interaction
+// because the outer component <YouTimeCol/>
+// handles it and passes the state down,
+// giving the user a larger click target.
 function Checkbox(props) {
   const {
     isDisabled,
     checkState,
   } = props;
 
+  // Store a mapping from the time slot state
+  // to the icons and CSS classes to display.
   const stateToComponent = {
-    0: <span/>,
-    1: <YesIcon/>,
-    2: <MaybeIcon/>,
+    [STATE.NO]: <span/>,
+    [STATE.YES]: <YesIcon/>,
+    [STATE.MAYBE]: <MaybeIcon/>,
   };
   const stateToClassName = {
-    0: "",
-    1: "yes-secondary",
-    2: "maybe-secondary",
+    [STATE.NO]: "",
+    [STATE.YES]: "yes-secondary",
+    [STATE.MAYBE]: "maybe-secondary",
   };
 
   return (
@@ -166,6 +218,8 @@ function Checkbox(props) {
   )
 }
 
+// Create a component to render the event info in the left
+// sidebar (description, organizer, time zone, etc).
 function EventInfo() {
   return (
     <div className="event-info">
@@ -197,16 +251,22 @@ function EventInfo() {
   );
 }
 
+// Define the top-level <App/> component
+// which is exported and rendered from ./main.jsx.
 function App() {
   return (
     <div>
+      {/* The app title */}
       <h1>Doodle</h1>
       <div className="app">
+        {/* The left event info sidebar. */}
         <EventInfo/>
+        {/* The right side contains the checkbox interactive interface. */}
         <div className="options-container">
           <div className="options-header">
             <h2>Select your preferred times</h2>
             <p>We'll let you know when the organizer picks the best time</p>
+            {/* There is a row of buttons for ordering the time slots and navigating/pagination. These buttons do not have click handlers but would in a full-featured implementation. */}
             <div className="button-row">
               <div className="buttons-left">
                 Order by <button disabled>Date</button> <button>Most popular</button>
@@ -218,26 +278,27 @@ function App() {
                 <button disabled>&rarr;</button>
               </div>
             </div>
-
           </div>
+          {/* The "body" of this right-hand side contains the time slot table. */}
           <div className="options-body">
+            {/* The top row of the body displays the date, time, and a checkbox for each slot. */}
             <div className="you-row options-row">
               <div className="name-col">
-                <span className="grow-el"></span>
+                <span className="grow-el"/>
                 <span>You</span>
               </div>
               {timeslots.map(timeslot => (
                 <YouTimeCol key={timeslot.start} start={timeslot.start} end={timeslot.end} />
               ))}
             </div>
+            {/* The middle row of the body displays the summary (number of people who are available) for each slot. */}
             <div className="summary-row options-row">
-              <div className="name-col">
-                
-              </div>
+              <div className="name-col" />
               {timeslots.map((timeslot) => (
                 <SummaryTimeCol key={timeslot.start} states={timeslot.states} start={timeslot.start} />
               ))}
             </div>
+            {/* The remaining rows of the body display each person's availability for each slot. */}
             {friends.map(friend => (
               <div className="friend-row options-row" key={friend}>
                 <div className="name-col">
@@ -250,6 +311,7 @@ function App() {
               </div>
             ))}
           </div>
+          {/* The footer of the interface contains more buttons. These buttons do not have click handlers but would in a full-featured implementation. */}
           <div className="options-footer">
             <div className="footer-row">
               You are the first participant to respond
